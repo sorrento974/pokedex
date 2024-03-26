0559d8a0a3755f01a191226ea1063184d48cf98e
kevin belin <sorrento974@gmail.com>
2024-03-26 14:42:25 +0100

    quete 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,27 @@
 function PokemonCard() {
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" /><figcaption>Bulbasaur</figcaption></figure>;
-}
-export default PokemonCard;
+    const pokemon = pokemonList[1];
+  
+    return (
+      <figure>
+        {pokemon.imgSrc ? ( 
+          <img src={pokemon.imgSrc} />
+        ) : (
+          <p>???</p> 
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  }
+  
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  export default PokemonCard;
