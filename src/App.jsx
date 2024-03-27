1132b18c35531e1b5a0f84dbb52515483ea74137
kevin belin <sorrento974@gmail.com>
2024-03-27 16:03:04 +0100

    quete 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import PokemonCard from "./components/PokemonCard"
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
-      <div>
-      <PokemonCard/>
-      </div> 
+    <>
+      <PokemonCard
+        pokemon={pokemonList[0]}
+      />
+    </>
   );
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,17 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1];
+function PokemonCard({pokemon}) {
   
     return (
       <figure>
-        {pokemon.imgSrc ? ( 
-          <img src={pokemon.imgSrc} />
+        {pokemon.image ? (
+          <img src={pokemon.image} alt={pokemon.name} />
         ) : (
-          <p>???</p> 
+          <p>???</p>
         )}
         <figcaption>{pokemon.name}</figcaption>
       </figure>
     );
   }
   
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  
   
   export default PokemonCard;
