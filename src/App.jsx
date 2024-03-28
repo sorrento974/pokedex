acc53b7d4d2a85013660f3034b50f58fc2c3f8d2
kevin belin <sorrento974@gmail.com>
2024-03-28 17:26:35 +0100

    quete 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import {useState} from "react";
 import PokemonCard from "./components/PokemonCard"
-
 
 function App() {
   const pokemonList = [
@@ -9,17 +9,50 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
-
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const before = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(Index => Index - 1);
+    }
+  };
+  const next = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(Index => Index + 1);
+    }
+  };
   return (
     <>
       <PokemonCard
-        pokemon={pokemonList[0]}
+        pokemon={pokemonList[pokemonIndex]}
       />
+       <button onClick={before} disabled={pokemonIndex === 0}>
+        Précédent
+      </button>
+      <button onClick={next} disabled={pokemonIndex === pokemonList.length - 1}>
+        Suivant
+      </button>
     </>
   );
 }
+    
+  
 
 export default App;
